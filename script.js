ffb63963cb39b618de309d4ba07a65379d3088db
dafahanafi1 <akunwifidafa@gmail.com>
2026-07-31 15:53:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function updateClock(){
 
-    let waktu = new Date();
-
-    let jam = String(waktu.getHours()).padStart(2,"0");
-
-    let menit = String(waktu.getMinutes()).padStart(2,"0");
-
-    let detik = String(waktu.getSeconds()).padStart(2,"0");
+let waktu = new Date();
 
 
-    document.getElementById("clock").innerHTML =
-    jam + ":" + menit + ":" + detik;
+let jam = String(waktu.getHours()).padStart(2,"0");
+
+let menit = String(waktu.getMinutes()).padStart(2,"0");
+
+let detik = String(waktu.getSeconds()).padStart(2,"0");
+
+
+document.getElementById("clock").innerHTML =
+jam + ":" + menit + ":" + detik;
+
 
 }
 
 
 updateClock();
-
 
 setInterval(updateClock,1000);
