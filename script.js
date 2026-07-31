431cd73eb7bc26fa37176d1eef54674cf6bac6b4
dafahanafi1 <akunwifidafa@gmail.com>
2026-07-31 13:55:47 +0700

    Refactor menu structure and update display logic

--- script.js
+++ script.js
@@ -1,76 +1,155 @@
-const links = [
-  {
-    title: "📋 Form ABSENSI KA SPPG, AK, AG, dan Relawan",
-    url: "https://sipgn-siphr.bgn.go.id/"
-  },
-  {
-    title: "📊 Laporan DIALUR / SMO",
-    url: "https://sipgn-sipsmo-web.bgn.go.id/Sign"
-  },
-  {
-    title: "🏢 SIPGN KA AK AG",
-    url: "https://sipgn.bgn.go.id/public"
-  },
-  {
-    title: "🧪 PIC Organoleptik ONLINE",
-    url: "https://pm-sipgn.bgn.go.id/auth/login"
-  },
-  {
-    title: "👥 MANAJEMEN PENERIMA MANFAAT",
-    url: "https://mpm-sipgn.bgn.go.id/"
-  },
-  {
-    title: "👨‍💼 POP khusus ASISTEN LAPANGAN",
-    url: "https://linktr.ee/dafahanafi"
-  },
-  {
-    title: "❤️ TAUWAS CARE",
-    url: "https://tauwascare.tauwas.bgn.go.id/login"
-  },
-  {
-    title: "🏦 MANDIRI KOPRA",
-    url: "https://koprabymandiri.com/"
-  },
-  {
-    title: "➕ TAMBAH RELAWAN ABSENSI ONLINE",
-    url: "https://sipgn-siphr.bgn.go.id/sign-in?callbackUrl=https%3A%2F%2Fsipgn-siphr.bgn.go.id%2Fmanajemen-tenaga-kerja%2Fkitchen-employees"
-  },
-  {
-    title: "👤 BIRO SDMO BGN",
-    url: "https://birosdmo.bgn.go.id/login"
-  },
-  {
-    title: "🛡️ SIPP BPJS KETENAGAKERJAAN",
-    url: "https://sipp.bpjsketenagakerjaan.go.id/"
-  }
+const menu = [
+
+{
+icon:"📋",
+nama:"Form ABSENSI KA SPPG, AK, AG, dan Relawan",
+link:"https://sipgn-siphr.bgn.go.id/"
+},
+
+{
+icon:"📊",
+nama:"Laporan DIALUR / SMO",
+link:"https://sipgn-sipsmo-web.bgn.go.id/Sign"
+},
+
+{
+icon:"🏢",
+nama:"SIPGN KA AK AG",
+link:"https://sipgn.bgn.go.id/public"
+},
+
+{
+icon:"🧪",
+nama:"PIC Organoleptik ONLINE",
+link:"https://pm-sipgn.bgn.go.id/auth/login"
+},
+
+{
+icon:"👨‍👩‍👧",
+nama:"MANAJEMEN PENERIMA MANFAAT",
+link:"https://mpm-sipgn.bgn.go.id/"
+},
+
+{
+icon:"🧑‍🏫",
+nama:"POP khusus ASISTEN LAPANGAN",
+link:"https://linktr.ee/dafahanafi"
+},
+
+{
+icon:"❤️",
+nama:"TAUWAS CARE",
+link:"https://tauwascare.tauwas.bgn.go.id/login"
+},
+
+{
+icon:"🏦",
+nama:"MANDIRI KOPRA",
+link:"https://koprabymandiri.com/"
+},
+
+{
+icon:"➕",
+nama:"TAMBAH RELAWAN ABSENSI ONLINE",
+link:"https://sipgn-siphr.bgn.go.id/sign-in?callbackUrl=https%3A%2F%2Fsipgn-siphr.bgn.go.id%2Fmanajemen-tenaga-kerja%2Fkitchen-employees"
+},
+
+{
+icon:"👤",
+nama:"BIRO SDMO BGN",
+link:"https://birosdmo.bgn.go.id/login"
+},
+
+{
+icon:"🛡️",
+nama:"SIPP BPJS KETENAGAKERJAAN",
+link:"https://sipp.bpjsketenagakerjaan.go.id/"
+}
+
 ];
 
-const menu = document.getElementById("menu");
-const search = document.getElementById("search");
+const menuBox=document.getElementById("menu");
 
-function tampilkanMenu(filter = "") {
+function tampil(data){
 
-  menu.innerHTML = "";
+menuBox.innerHTML="";
 
-  links
-    .filter(item =>
-      item.title.toLowerCase().includes(filter.toLowerCase())
-    )
-    .forEach(item => {
+data.forEach(item=>{
 
-      menu.innerHTML += `
-      <a class="link" href="${item.url}" target="_blank">
-          <span>${item.title}</span>
-          <i class="fa-solid fa-arrow-up-right-from-square"></i>
-      </a>
-      `;
+menuBox.innerHTML+=`
 
-    });
+<a class="menu-item"
+
+href="${item.link}"
+
+target="_blank">
+
+<div>
+
+${item.icon}
+
+&nbsp;
+
+${item.nama}
+
+</div>
+
+<span>
+
+<i class="fa-solid fa-arrow-up-right-from-square"></i>
+
+</span>
+
+</a>
+
+`;
+
+});
+
+document.getElementById("jumlahMenu").innerHTML=data.length;
 
 }
 
-tampilkanMenu();
+tampil(menu);
 
-search.addEventListener("input", function () {
-  tampilkanMenu(this.value);
+document.getElementById("search").addEventListener("keyup",function(){
+
+const keyword=this.value.toLowerCase();
+
+const hasil=menu.filter(item=>
+
+item.nama.toLowerCase().includes(keyword)
+
+);
+
+tampil(hasil);
+
 });
+
+const popup=document.getElementById("popup");
+
+const tombol=document.getElementById("lihatQR");
+
+const close=document.getElementById("close");
+
+tombol.onclick=()=>{
+
+popup.style.display="flex";
+
+}
+
+close.onclick=()=>{
+
+popup.style.display="none";
+
+}
+
+popup.onclick=(e)=>{
+
+if(e.target===popup){
+
+popup.style.display="none";
+
+}
+
+}
