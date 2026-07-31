c17374cea4ac778745266de458d820d8b24dabe4
dafahanafi1 <akunwifidafa@gmail.com>
2026-07-31 15:34:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Portal MBG aktif");
+function updateClock(){
+
+    let waktu = new Date();
+
+
+    let jam = String(waktu.getHours()).padStart(2,"0");
+
+    let menit = String(waktu.getMinutes()).padStart(2,"0");
+
+    let detik = String(waktu.getSeconds()).padStart(2,"0");
+
+
+    document.getElementById("clock").innerHTML =
+    jam + ":" + menit + ":" + detik;
+
+}
+
+
+setInterval(updateClock,1000);
+
+
+updateClock();
