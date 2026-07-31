83088251244abf96c4908d536fa686a0598ececd
dafahanafi1 <akunwifidafa@gmail.com>
2026-07-31 14:48:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Portal MBG aktif");
+// ===================================
+// PORTAL MBG SPPG KARANGWARU 2
+// JAVASCRIPT UTAMA
+// ===================================
+
+
+console.log("Portal MBG berhasil dimuat");
+
+
+
+// Tahun otomatis footer
+const tahun = new Date().getFullYear();
+
+const footer = document.querySelector("footer");
+
+if (footer) {
+
+    footer.innerHTML =
+        "Portal MBG SPPG Karangwaru 2 © " + tahun;
+
+}
