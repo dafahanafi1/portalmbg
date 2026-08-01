eb0cbf523a19bfafc35526c50becc58570746431
dafahanafi1 <akunwifidafa@gmail.com>
2026-08-02 00:49:19 +0700

    Update Portal MBG v2

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-function updateClock(){
+function updateClock() {
 
-let waktu=new Date();
+const waktu = new Date();
 
-let jam=String(waktu.getHours()).padStart(2,"0");
-
-let menit=String(waktu.getMinutes()).padStart(2,"0");
-
-let detik=String(waktu.getSeconds()).padStart(2,"0");
-
+const jam = String(waktu.getHours()).padStart(2,"0");
+const menit = String(waktu.getMinutes()).padStart(2,"0");
+const detik = String(waktu.getSeconds()).padStart(2,"0");
 
 document.getElementById("clock").innerHTML =
-jam+":"+menit+":"+detik;
+jam + ":" + menit + ":" + detik;
 
 }
 
-
 updateClock();
-
 setInterval(updateClock,1000);
+
+
+
+function toggleTutorial(){
+
+const menu = document.getElementById("tutorialMenu");
+
+if(menu.style.display==="block"){
+
+menu.style.display="none";
+
+}else{
+
+menu.style.display="block";
+
+}
+
+}
