25e2c55456168d84ebe6cb34c4e0ce08bb6a1a2a
dafahanafi1 <akunwifidafa@gmail.com>
2026-08-04 01:21:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-// JAM DIGITAL
-
 function updateClock(){
-
-const now = new Date();
-
-let h = String(now.getHours()).padStart(2,'0');
-let m = String(now.getMinutes()).padStart(2,'0');
-let s = String(now.getSeconds()).padStart(2,'0');
-
-document.getElementById("clock").innerHTML = `${h}:${m}:${s}`;
-
+    const now=new Date();
+    const h=String(now.getHours()).padStart(2,'0');
+    const m=String(now.getMinutes()).padStart(2,'0');
+    const s=String(now.getSeconds()).padStart(2,'0');
+    document.getElementById("clock").innerHTML=`${h}:${m}:${s}`;
 }
-
 setInterval(updateClock,1000);
-
 updateClock();
 
-
-// DROPDOWN TUTORIAL
-
 function toggleTutorial(){
-
-const menu = document.getElementById("tutorialMenu");
-
-if(menu.style.display==="block"){
-
-menu.style.display="none";
-
-}else{
-
-menu.style.display="block";
-
-}
-
+    const menu=document.getElementById("tutorialMenu");
+    if(menu.style.display==="block"){
+        menu.style.display="none";
+    }else{
+        menu.style.display="block";
+    }
 }
